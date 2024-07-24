f7d9c5e9a4b37048a1066552a6eafae0f2507e07
Shivam Singh <60600461+shivamsi0006@users.noreply.github.com>
2024-07-24 15:25:57 +0530

    Update __init__.py
    change host - docker.host.internal

--- my_database_lib/__init__.py
+++ my_database_lib/__init__.py
@@ -1,7 +1,7 @@
 # my_database_lib/__init__.py
 from flask_sqlalchemy import SQLAlchemy
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@host.docker.internal:5432/database'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/database'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "secretkey"
 
